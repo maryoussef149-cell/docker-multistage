385c7ee33c50577a609ebd430aee3604e2455118
Mariam <maryoussef149@gmail.com>
2026-07-21 15:07:43 +0300

    feat: add express server with health check

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('CI/CD pipeline is running!');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+	res.send('Hello from myapp!');
+});
+
+app.get('/health', (req, res) => {
+	res.status(200).json({ status: 'ok'});
+});
+
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
